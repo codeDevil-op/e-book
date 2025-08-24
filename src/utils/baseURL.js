24bbd36c9c68faccb66c3a0f152bc68d1df440ff
NightMare <rahmanishfaq146@gmail.com>
2025-08-24 17:23:20 +0500

    updated repo

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,8 +1,8 @@
-// const baseUrl =()=>{
-//     return "http://localhost:5000"
-// }
+const baseUrl =()=>{
+    return "http://localhost:5000"
+}
 
-const baseUrl = "http://localhost:5000"
+// const baseUrl = "http://localhost:5000"
 
 
 export default baseUrl
